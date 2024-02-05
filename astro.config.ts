85f38f3bc15560587dad54847513e3e0186bd019
Atharva Pise <atharvapise19@gmail.com>
2024-02-06 03:09:42 +0530

    feat: integrate vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,25 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://atharvatypes.vercel.app'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3000/',
+  site: process.env.VERCEL_ENV === 'production' ? 'https://atharvatypes.vercel.app' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://localhost:3000/',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  })],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
+      exclude: ['@resvg/resvg-js']
+    }
   },
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
