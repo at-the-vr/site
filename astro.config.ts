fb83e5d360b0cab63359510a4d7cff7f4f8ee215
Atharva Pise <atharvapise19@gmail.com>
2024-02-06 01:37:43 +0530

    chore: change domain name

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
-      ? 'https://atharva.types.vercel.app'
+      ? 'https://atharvatypes.vercel.app'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3000/',
